2f73ae8a3c01273df2a12de32d83b0fc59d3458f
gabgab1337 <gabrielhanszke@gmail.com>
2023-12-07 14:14:57 +0100

    gitignore update

--- Algorytmy/NWD&NWW.cpp
+++ Algorytmy/NWD&NWW.cpp
@@ -1,20 +1,23 @@
+// Algorytm iteracyjny obliczania największego wspólnego dzielnika (NWD)
 int NWD_iter(int a, int b){
     while(a != b){
         if(a > b)
-            a -= b;
+            a -= b; // Jeśli a > b, odejmujemy b od a
         else
-            b -= a;
+            b -= a; // W przeciwnym razie odejmujemy a od b
     }
-    return a;
+    return a; // Zwracamy a (które jest NWD)
 }
 
+// Algorytm rekurencyjny obliczania największego wspólnego dzielnika (NWD)
 int NWD_rek(int a, int b){
     if(b == 0)
-        return a;
+        return a; // Jeśli b jest zerem, zwracamy a (ostatnie niezerowe a)
     else
-        return NWD_rek(b, a % b);
+        return NWD_rek(b, a % b); // W przeciwnym razie wywołujemy rekurencyjnie dla (b, a % b)
 }
 
+// Funkcja obliczająca najmniejszą wspólną wielokrotność (NWW)
 int NWW(int a, int b){
-    return a * b / NWD_iter(a, b);
+    return a * b / NWD_iter(a, b); // Obliczamy NWW za pomocą iteracyjnie lub rekurencyjnie obliczonego NWD
 }
--- Algorytmy/NWD&NWW.py
+++ Algorytmy/NWD&NWW.py
@@ -1,13 +1,16 @@
+# Iteracyjny algorytm obliczania największego wspólnego dzielnika (NWD)
 def NWD_iter(a, b):
     while b != 0:
-        a, b = b, a % b
-    return a
+        a, b = b, a % b  # Zastępuje a przez b, a b przez resztę z dzielenia a przez b
+    return a  # Zwraca a (które jest NWD)
 
+# Rekurencyjny algorytm obliczania największego wspólnego dzielnika (NWD)
 def NWD_rek(a, b):
     if b == 0:
-        return a
+        return a  # Jeśli b jest zerem, zwraca a (ostatnie niezerowe a)
     else:
-        return NWD_rek(b, a % b)
-    
+        return NWD_rek(b, a % b)  # W przeciwnym razie wywołuje rekurencyjnie dla (b, a % b)
+
+# Funkcja obliczająca najmniejszą wspólną wielokrotność (NWW)
 def NWW(a, b):
-    return a * b / NWD_iter(a, b)
+    return a * b / NWD_iter(a, b)  # Oblicza NWW za pomocą iteracyjnie lub rekurencyjnie obliczonego NWD
